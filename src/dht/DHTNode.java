72e01d70ed445dbfc83a8d6635228fdf67637dc2
Deepfreeze32 <tcc10a@acu.edu>
2013-04-14 01:27:39 -0500

    Found a possible solution.

--- src/dht/DHTNode.java
+++ src/dht/DHTNode.java
@@ -146,8 +146,8 @@ public class DHTNode extends Thread {
                             }
                             out.write(x);
                         }
-                        out.close();
-                        break;
+                        out.flush();
+                        //break;
                         //continue;
                     }
                 } else if (clientMessage.contains("insert")) {
